1245852e79ecc3422ffd8c7a2d0cd408c590b019
nbenitez <nbenitezl@gmail.com>
2011-01-25 01:12:52 -0800

    Fix render event when timeslotsPerHour is 1 If the slot is big enough (>25px) we show the two lines of text (Event time and title), if not then do current behaviour of only showing one line (event time).

--- jquery.weekcalendar.js
+++ jquery.weekcalendar.js
@@ -1005,7 +1005,11 @@
 
                var $newEvent = $("<div class=\"wc-cal-event wc-new-cal-event wc-new-cal-event-creating\"></div>");
 
-               $newEvent.css({lineHeight: (options.timeslotHeight - 2) + "px", fontSize: (options.timeslotHeight / 2) + "px"});
+               if (options.timeslotsPerHour == 1 && options.timeslotHeight > 25) {
+                  $newEvent.css({lineHeight: ((options.timeslotHeight / 2)-1) + "px", fontSize: (options.timeslotHeight / 4) + "px"});
+               } else {
+                  $newEvent.css({lineHeight: (options.timeslotHeight - 2) + "px", fontSize: (options.timeslotHeight / 2) + "px"});
+               }
                $target.append($newEvent);
 							 
                var columnOffset = $target.offset().top;
